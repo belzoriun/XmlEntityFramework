5aa7f77df258a8b933d3fdda31cc11f2c122b00d
Florian BOYER <florian.boyer@imerir.com>
2021-05-29 14:52:41 +0200

    Ajoutez des fichiers projet.

---
+++ XmlEntityFramework/XmlParser/XmlParser.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XmlEntityFramework.XmlParser
+{
+    class XmlParser
+    {
+        public void ParseFromString(string xmlData)
+        {
+            XDocument doc = XDocument.Parse(xmlData); //or XDocument.Load(path)
+            string jsonText = JsonConvert.SerializeXNode(doc);
+            dynamic dyn = JsonConvert.DeserializeObject<ExpandoObject>(jsonText);
+        }
+
+        public void ParseFromFile(string path)
+        {
+
+        }
+    }
+}
